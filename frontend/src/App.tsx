c6a7b11ab6417871cf7dd3f94ad0b9d9a88d8e7a
Vo Hoang Phuc <22130222@st.hcmuaf.edu.vn>
2026-02-23 15:31:17 +0700

    Them thu vien tailwindcss

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,13 @@
-function App() {
+import './index.css' 
 
+function App() {
   return (
-    <>
-     <h1>Hello</h1>
-    </>
-  )
+    <div className="min-h-screen bg-gradient-to-r from-blue-500 to-purple-600 flex items-center justify-center">
+      <h1 className="text-6xl font-extrabold text-white drop-shadow-lg animate-pulse">
+        Tailwind CSS ĐÃ CÀI THÀNH CÔNG!
+      </h1>
+    </div>
+  );
 }
 
-export default App
+export default App;
